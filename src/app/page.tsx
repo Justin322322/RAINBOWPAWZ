240e37e477aa25f98a7b0b3a7addd5afddf2db0d
Justin322322 <justinmarlosibonga@gmail.com>
2025-09-15 04:13:06 +0800

    added showcase remove dots and refactored text font

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -357,7 +357,7 @@ export default function Home() {
 
       {/* Hero Section */}
       <section id="hero" className="relative min-h-screen w-full overflow-hidden">
-        <div className="absolute inset-0">
+        <div className="fixed inset-0 -z-10">
           <video
             autoPlay
             loop
