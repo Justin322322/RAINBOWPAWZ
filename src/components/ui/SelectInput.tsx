8214f3ef8ad23a798e948aa92b41f3c0dfabd79b
Justin322322 <justinmarlosibonga@gmail.com>
2025-10-04 10:54:47 +0800

    added option to change biz ent/type

--- src/components/ui/SelectInput.tsx
+++ src/components/ui/SelectInput.tsx
@@ -102,7 +102,7 @@ const SelectInput = forwardRef<HTMLDivElement, SelectInputProps>(
           </button>
           
           {isOpen && !disabled && (
-            <div className="absolute z-10 w-full mt-1 bg-white border border-gray-300 rounded-md shadow-lg max-h-60 overflow-auto">
+            <div className="absolute z-50 w-full mt-1 bg-white border border-gray-300 rounded-md shadow-lg max-h-60 overflow-auto">
               <ul className="py-1">
                 {options.map((option) => (
                   <li key={option.value}>
